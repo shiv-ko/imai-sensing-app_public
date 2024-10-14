0703b304d80b623f1dcd66cd3567e7ce173797dd
tsukasa1111 <tsukasa1257@example.com>
2024-10-15 03:47:21 +0900

    fix: 未使用無視検証

--- src/features/dashboard/utils/awsService.ts
+++ src/features/dashboard/utils/awsService.ts
@@ -124,6 +124,7 @@ export const updateUserData = async (input: UpdateUserMutationVariables['input']
 
 
 function stripTypename<T extends { __typename?: string }>(obj: T): Omit<T, '__typename'> {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
  const { __typename, ...rest } = obj;
  return rest;
 }
