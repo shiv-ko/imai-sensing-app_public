a4de0727cfb3d4acc9ecf8f5e1424bb9e8cd63fb
Issey M <isseymatsumoto0911@gmail.com>
2024-10-17 00:38:57 +0900

    remove comment

--- src/shared/utils/category/categorydownMenu.tsx
+++ src/shared/utils/category/categorydownMenu.tsx
@@ -1,34 +1,3 @@
-// import React from 'react';
-// import Select from 'react-select';
-
-
-// interface CategoryDropdownProps {
-//   selectedCategory: string;
-//   categories: string[];
-//   onCategoryChange: (category: string) => void;
-// }
-
-// const CategoryDropdown: React.FC<CategoryDropdownProps> = ({ selectedCategory, categories, onCategoryChange }) => {
-//   return (
-//     <div>
-//       <label htmlFor="category">Category: </label>
-//       <select
-//         id="category"
-//         value={selectedCategory}
-//         onChange={(e) => onCategoryChange(e.target.value)}
-//       >
-        
-//         {categories.map((category) => (
-//           <option key={category} value={category}>
-//             {category}
-//           </option>
-//         ))}
-//       </select>
-//     </div>
-//   );
-// };
-
-// export default CategoryDropdown;
 
 import React from 'react';
 import Select from 'react-select';
