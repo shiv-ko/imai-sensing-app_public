3b7fe212c041b21b5eaf2ea02e94452a430f99a9
shiv-ko <iscopre7@gmail.com>
2024-10-17 10:04:02 +0900

    change: ロゴの変更 feat: ログインページでのルーティング周りを変更。 fix: デバッグ表示の修正

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,44 +1,43 @@
-
-'use client'
+'use client';
 import { Authenticator } from '@aws-amplify/ui-react';
+import { fetchUserAttributes} from 'aws-amplify/auth';
 import { Amplify } from 'aws-amplify';
 import React, { useState, useEffect } from 'react';
 import awsExports from './../../aws-exports';
-import Header from '@/shared/components/header'; // Headerコンポーネントのインポート
-import FooterNavBar from '@/shared/components/footer'; // FooterNavBarコンポーネントのインポート
 import '@aws-amplify/ui-react/styles.css';
+import { useRouter } from 'next/navigation';
+
 Amplify.configure({ ...awsExports });
 
 const formFields = {
   signUp: {
     email: {
       label: 'メールアドレス',
-      order: 2
+      order: 2,
     },
     password: {
       label: 'パスワード',
-      order: 3
-    }
-  }
+      order: 3,
+    },
+  },
 };
 
-
 export default function App() {
-
   const [locationGranted, setLocationGranted] = useState(false);
-  const [position, setPosition] = useState({ latitude: 0, longitude: 0 }); 
   const [errorMessage, setErrorMessage] = useState('');
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const [displayName, setDisplayName] = useState('');
+  const router = useRouter();
 
   useEffect(() => {
     // ページに入ったときに位置情報を取得
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        setLocationGranted(true); // 位置情報が取得できたらフラグをtrueに
-        setPosition({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        }); // 緯度と経度を保存
-
+        if (position) {
+          // 位置情報が取得できたらフラグをtrueに
+          setLocationGranted(true);
+          console.log(position);
+        }
       },
       (error) => {
         console.error(error);
@@ -47,33 +46,67 @@ export default function App() {
       }
     );
   }, []);
-  
-  
+
+  const handleButtonClick = async () => {
+    try {
+      const currentUser =await fetchUserAttributes();
+      console.log(currentUser);
+      const username = currentUser.nickname|| '' 
+      setDisplayName(username);
+      setButtonClicked(true);
+      setTimeout(() => {
+        router.push('/home');
+      }, 2000); // 2秒後にリダイレクト
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div>
+    <div style={styles.container}>
       {locationGranted ? (
-        <>
-        <Authenticator formFields={formFields} socialProviders={['amazon', 'apple', 'facebook', 'google']}>
-          {({ signOut, user }) => (
-            <main>
-              <Header></Header>
-              <h1>こんにちは、{user?.username} さん</h1>
-              <button onClick={signOut}>サインアウト</button>
-              <FooterNavBar></FooterNavBar>
-            </main>
+        <Authenticator formFields={formFields}>
+          {() => (
+            <div>
+              {!buttonClicked ? (
+                <button style={styles.button} onClick={handleButtonClick}>
+                  今井町について知る
+                </button>
+              ) : (
+                <div>
+                  <h1>ようこそ、{displayName} さん</h1>
+                </div>
+              )}
+            </div>
           )}
         </Authenticator>
-        {/* 取得した位置情報を表示 */}
-          <div>
-            <h2>現在の位置情報</h2>
-            <p>緯度: {position.latitude}</p>
-            <p>経度: {position.longitude}</p>
-          </div>
-        </>
       ) : (
-        <p>{errorMessage || '位置情報を確認中です。'}</p>
+        <p style={styles.errorMessage}>{errorMessage || '位置情報を確認中です。'}</p>
       )}
-
     </div>
   );
+}
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column' as const,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    backgroundColor: '#f4f4f4',
+  },
+  errorMessage: {
+    color: 'red',
+    fontSize: '1.2rem',
+  },
+  button: {
+    padding: '0.5rem 1rem',
+    backgroundColor: '#ff6f61',
+    border: 'none',
+    color: 'white',
+    cursor: 'pointer',
+    borderRadius: '4px',
+    fontSize: '1rem',
+  },
 };
