0f96d13fa691afb71c8ef9a540cf8611ab2a7efc
tsukasa1111 <tsukasa1257@example.com>
2024-11-02 02:52:57 +0900

    fix: スクロールするとヘッダが移動するバグを修正

--- src/app/camera/page.tsx
+++ src/app/camera/page.tsx
@@ -1,15 +1,26 @@
-
 'use client'
 import React from 'react';
-import Header from '@/shared/components/header'; // Headerコンポーネントのインポート
-import FooterNavBar from '@/shared/components/footer'; // FooterNavBarコンポーネントのインポート
+import Header from '@/shared/components/header';
+import FooterNavBar from '@/shared/components/footer';
 import ThemeSelectPage from '@/features/post/pages/themeSelectPage';
 
 const PostFormPage: React.FC = () => {
   return (
-    <div>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: '100vh',
+      position: 'relative',
+      backgroundColor: '#F5F5F5',
+    }}>
       <Header />
-      <ThemeSelectPage></ThemeSelectPage>
+      <main style={{
+        flex: 1,
+        marginTop: '60px',
+        marginBottom: '60px',
+      }}>
+        <ThemeSelectPage />
+      </main>
       <FooterNavBar />
     </div>
   );
