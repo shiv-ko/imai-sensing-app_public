ba79b0b0af0be34e2510a065abf9aa99560d9792
shiv-ko <iscopre7@gmail.com>
2024-11-08 14:34:45 +0900

    feat: 運用ポリシーと使い方を見れるボタンを追加

--- src/shared/components/header.tsx
+++ src/shared/components/header.tsx
@@ -2,36 +2,51 @@
 import { useRouter } from 'next/navigation';
 import { signOut } from 'aws-amplify/auth';
 
-
 const Header = () => {
   const router = useRouter();
-  
-
 
   return (
-  <header style={styles.header}>
-    <div style={styles.logo}>
-      <img src="/logo.png" alt="Logo" style={styles.logoImage} />
-      <h1 style={styles.title}>i-MYGO</h1>
-    </div>
-    <div style={styles.buttons}>
-      {/* 使い方ページに移動するボタン */}
+    <header style={styles.header}>
+      <div style={styles.logo}>
+        <img src="/logo.png" alt="Logo" style={styles.logoImage} />
+        <h1 style={styles.title}>i-MYGO</h1>
+      </div>
+      <div style={styles.buttons}>
+        {/* 利用規約ボタン */}
+        <button
+          style={styles.linkButton}
+          onClick={() => {
+            window.open('/運用ポリシー.pdf', '_blank');
+          }}
+        >
+          利用規約
+        </button>
 
-      <button
-        style={styles.button}
-        onClick={() => {
-          if (signOut && router) { // signOut と router が定義されているか確認
-            signOut(); // サインアウト
-            router.push('/signin'); // サインアウト後に /signin にリダイレクト
-          }
-        }}
-      >
-        サインアウト
-      </button>
-    </div>
-  </header>
-);
+        {/* 使い方ボタン */}
+        <button
+          style={styles.linkButton}
+          onClick={() => {
+            window.open('http://watery-literature-3ab.notion.site', '_blank'); // 外部リンクに遷移
+          }}
+        >
+          使い方
+        </button>
 
+        {/* サインアウトボタン */}
+        <button
+          style={styles.button}
+          onClick={() => {
+            if (signOut && router) {
+              signOut();
+              router.push('/signin');
+            }
+          }}
+        >
+          サインアウト
+        </button>
+      </div>
+    </header>
+  );
 };
 
 // CSS-in-JSスタイル
@@ -53,17 +68,27 @@ const styles = {
     marginRight: '10px',
   },
   title: {
-    fontSize: '1.5rem',
+    fontSize: '1.3rem',
     color: '#333',
   },
   buttons: {
     display: 'flex',
-    gap: '10px',
+    gap: '5px',
+  },
+  linkButton: {
+    padding: '2px 4px', // 余白を小さくする
+    fontSize: '0.8rem', // フォントサイズを小さくする
+    backgroundColor: 'transparent',
+    color: '#333',
+    border: 'none',
+    cursor: 'pointer',
+    textDecoration: 'underline',
+    lineHeight: '1', // ボタン内のテキストの高さを調整
   },
   button: {
-    padding: '10px',
+    padding: '3px',
     fontSize: '1rem',
-    backgroundColor: '#4CAF50', // 緑色のボタン
+    backgroundColor: '#4CAF50',
     color: '#fff',
     border: 'none',
     borderRadius: '4px',
